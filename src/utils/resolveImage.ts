c84c984ef1d9dcec36ca051fb31c55aefa6d1818
Elijan Mastnak <git@ejmastnak.com>
2025-10-28 22:00:59 +0100

    more image changes

--- src/utils/resolveImage.ts
+++ src/utils/resolveImage.ts
@@ -1,8 +1,9 @@
 import type { ImageMetadata } from "astro";
-import { TINA_MEDIA_ROOT } from "@/config";
+import { TINA_MEDIA_ROOT, } from "@/config";
 
+// TODO: has to be hardcoded!
 const localImages = import.meta.glob<{ default: ImageMetadata }>(
-  "/src/assets/img/*.{jpg,jpeg,png,webp,avif}",
+  "/src/assets/img/uploads/**/*.{jpg,jpeg,png,webp}",
   { eager: true }
 );
 
@@ -12,23 +13,20 @@ export async function resolveTinaImage(
   if (!tinaPath) return null;
 
   // Map remote Tina Cloud asset to corresponding local file
-  // Takes e.g. https://assets.tina.io/b2c21e1d-b922-462e-99fa-06c8a69a84fb/img/apple.jpg
-  // ...extracts img/apple.jpg, and maps to src/assets
-  if (tinaPath.startsWith("https://assets.tina.io")) {
-    const match = tinaPath.match(/\/(img\/.+)$/);
-    if (match) {
-      const rel = match[1];
-      const key = `${TINA_MEDIA_ROOT}/${rel}`;
-      const found = localImages[key];
-      if (found) return found.default;
-    }
+  // Takes e.g. https://assets.tina.io/b2c21e1d-b922-462e-99fa-06c8a69a84fb/foo/apple.jpg
+  // ...extracts foo/apple.jpg, then maps to media folder
+  const TINA_ASSETS_PREFIX = /^https:\/\/assets\.tina\.io\/[^/]+\/(.+)$/;
+  const cloudMatch = tinaPath.match(TINA_ASSETS_PREFIX);
+  if (cloudMatch) {
+    const relPath = cloudMatch[1];
+    const key = `${TINA_MEDIA_ROOT}/${relPath}`;
+    const match = localImages[key];
+    if (match) return match.default;
     return null;
   }
 
-  // Map local images to `/src/assets`, e.g. /img/foo.jpg -> /src/assets/img/foo.jpg
-  const cleanPath = tinaPath.replace(/^\/+/, "");  // remove leading slash if present
-  const key = `${TINA_MEDIA_ROOT}/${tinaPath}`;
-  const match = localImages[key];
+  // Local images are already in full asset path form, e.g. /src/assets/img/foo.jpg
+  const match = localImages[tinaPath];
   if (match) return match.default;
 
   return null;
